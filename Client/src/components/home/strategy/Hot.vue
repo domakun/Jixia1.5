e15b21fc625e1892f7fa74d120c384f27ba2926a
KAng <1365271643@qq.com>
2018-12-26 09:21:10 +0800

    主页面攻略视图

---
+++ Client/src/components/home/strategy/Hot.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="hot">
+		<div class="tab_cont" id="tabs-1">
+			<img src="http://n3-q.mafengwo.net/s12/M00/36/7B/wKgED1vj_cqABBXTAAKeLyNKj0442.jpeg?imageMogr2%2Fthumbnail%2F%21320x170r%2Fgravity%2FCenter%2Fcrop%2F%21320x170%2Fquality%2F100">
+			<div class="strategy">
+				<div class="title">
+					冬天来成都滑雪、泡温泉，上西岭雪山嗨个够
+				</div>
+				<div class="glb_info">
+					西岭雪山分为前山和后山，目前开发比较完善的是后山，滑雪场和滑草场都在这里。
+					如果你是喜欢徒步攀登的“驴友”，可以尝试一下前山，风景虽然比不上后山，但也别有一番情趣。
+				</div>
+				<a href="#" class="btn">Details</a>
+				<div class="clear"></div>
+			</div>
+			<div style="width: 100%;height: 20px;"></div>
+			<img src="http://p4-q.mafengwo.net/s11/M00/5C/EE/wKgBEFrwgp6AcMiaAAF_KAgInMU56.jpeg?imageMogr2%2Fthumbnail%2F%21220x150r%2Fgravity%2FCenter%2Fcrop%2F%21220x150%2Fquality%2F90" alt="">
+			<div class="strategy">
+				<div class="title">
+					用吃货的方式打开成都的一天，是这样的
+				</div>
+				<div class="glb_info">
+					虽然老话说：“少不入川，老不出蜀”，但我觉得这个时代恰恰相反。少要入川，老要出蜀，才是正道。
+				</div>
+				<a href="#" class="btn">Details</a>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Hot',
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+	
+</style>
