410480890965a15e7bc776fcf748367bf5131261
邓烈丽 <872672707@qq.com>
2018-12-29 11:04:50 +0800

    评论区

--- Client/build/webpack.base.conf.js
+++ Client/build/webpack.base.conf.js
@@ -89,8 +89,7 @@ module.exports = {
     net: 'empty',
     tls: 'empty',
     child_process: 'empty'
-  }
-},
+  },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin('common.js'),
     new webpack.ProvidePlugin({
